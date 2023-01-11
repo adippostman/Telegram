f3034a57a63fbb24c40d1d7a6f861fedb7358997
adippostman <dhfmedia.blog@gmail.com>
2023-01-11 20:55:16 +0700

    haha

--- telebot.js
+++ telebot.js
@@ -1,5 +1,6 @@
 const { Telegraf, Markup } = require("telegraf");
 const { Spot } = require(`@binance/connector`);
+const fs = require(`fs`);
 require(`dotenv`).config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN_NEW);
@@ -36,19 +37,38 @@ const adminId = 1080449574;
 
     bot.action(`getCryptoPrice`, async (ctx) => {
       try {
-        const binance = new Spot(
-          process.env.BINANCE_API_KEY,
-          process.env.BINANCE_SECRET_KEY
-        );
-        await ctx.deleteMessage();
-        const btcPrice = await binance.ticker24hr(`BTCUSDT`);
-        const priceChangePercent = parseFloat(btcPrice.data.lastPrice).toFixed(
-          2
-        );
-        const color = priceChangePercent < 0 ? `🔴` : `🟢`;
-        await ctx.sendMessage(
-          `Ini adalah data harga Cryptocurrencies yang diambil dari data Binance API secara realtime:\n\n` +
-            `${color} ${btcPrice.data.symbol} | $${priceChangePercent} USDT | ${btcPrice.data.priceChangePercent} %`
+        // const binance = new Spot(
+        //   process.env.BINANCE_API_KEY,
+        //   process.env.BINANCE_SECRET_KEY
+        // );
+        // await ctx.deleteMessage();
+        // const btcPrice = await binance.ticker24hr(`BTCUSDT`);
+        // const priceChangePercent = parseFloat(btcPrice.data.lastPrice).toFixed(
+        //   2
+        // );
+        const file = JSON.parse(fs.readFileSync(`./coingecko.json`, `utf-8`));
+        let message = ``;
+        let count = 0;
+        for (const obj of file.data) {
+          if (
+            obj.symbol == `usdt` ||
+            obj.symbol == `usdc` ||
+            obj.symbol == `busd`
+          ) {
+            continue;
+          }
+          count++;
+          if (count > 10) {
+            break;
+          }
+          let color = obj.market_data.price_change_24h < 0 ? `🔴` : `🟢`;
+          message += `${color} <b>${obj.symbol.toUpperCase()}USD</b> | $${
+            obj.market_data.current_price.usd
+          } USD | ${obj.market_data.price_change_percentage_24h}%\n\n`;
+        }
+        await ctx.replyWithHTML(
+          `Ini adalah data harga Cryptocurrencies yang diambil dari data CoinGecko API secara realtime:\n\n` +
+            message
         );
       } catch (err) {
         console.error(err);
